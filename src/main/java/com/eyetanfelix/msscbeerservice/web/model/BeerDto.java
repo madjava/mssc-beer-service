97ba9323cc8b26d58b593519a4c8c5f8c9f7d7b6
Felix Eyetan <peyetan@yahoo.com>
2020-04-24 22:59:18 +0100

    Adding Bean Validation Close #8

--- src/main/java/com/eyetanfelix/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/eyetanfelix/msscbeerservice/web/model/BeerDto.java
@@ -4,7 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.aspectj.lang.annotation.Pointcut;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,13 +19,32 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
+
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEmun beerStyle;
+
+    @Positive
+    @NotNull
     private Long upc;
+
+    @Positive
+    @NotNull
     private BigDecimal price;
+
     private Integer quantityOnHand;
 }
