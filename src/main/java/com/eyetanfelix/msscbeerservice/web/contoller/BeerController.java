ec610e92e33971d9d89160fc47dd825dcfb47eab
Felix Eyetan <peyetan@yahoo.com>
2020-04-24 11:12:36 +0100

    Create Beer Service Controller #3

---
+++ src/main/java/com/eyetanfelix/msscbeerservice/web/contoller/BeerController.java
@@ -0,0 +1,31 @@
+package com.eyetanfelix.msscbeerservice.web.contoller;
+
+import com.eyetanfelix.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/api/v1/beer")
+public class BeerController {
+
+    @GetMapping(value = "/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerID) {
+        //TODO impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+
+        return new ResponseEntity(HttpStatus.CREATED);
+
+    }
+
+    @PutMapping(value = "/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
